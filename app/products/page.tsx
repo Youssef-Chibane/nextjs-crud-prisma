3617594527ea44038223109cb830521ac1c65af1
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-07 14:01:42 +0000

    products count

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,11 +2,19 @@ import { prisma } from "../lib/prisma";
 import Link from "next/link";
 
 export default async function ProductsPage() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({
+    orderBy: {
+      createAt: "desc",
+    },
+  });
+
+  const productsCount = await prisma.product.count();
 
   return (
     <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-gray-900 mb-6">All Products</h1>
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">
+        All Products ({productsCount})
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <div
